653e7be4a38829351616e9f556b45da3a32b5c18
Christopher Brumm <30894952+crmhh@users.noreply.github.com>
2021-06-18 13:07:05 +0200

    Create reset-guests.ps1

---
+++ reset-guests.ps1
@@ -0,0 +1,12 @@
+# You'll need a fresh version of the module
+Uninstall-Module AzureADPreview
+Install-Module AzureADPreview
+Connect-AzureAD
+
+# Var def
+$mydomain = "*example*"
+$myguests = Get-AzureADUser -All 1| Where-Object {$_.Usertype -eq "Guest"} | Where-Object {$_.UserPrincipalName -like $mydomain}
+foreach ($guest in $myguests) {
+  $msGraphUser = New-Object Microsoft.Open.MSGraph.Model.User -ArgumentList $guest.ObjectId
+  New-AzureADMSInvitation -InvitedUserEmailAddress $guest.mail -SendInvitationMessage $True -InviteRedirectUrl "http://myapps.microsoft.com" -InvitedUser $msGraphUser -ResetRedemption $True
+}
